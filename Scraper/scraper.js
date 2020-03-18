c7921c0a3de83b11430f0b2373d7334fbfbf55e5
Kasper Leander Helmer Pedersen <kasper0207@gmail.com>
2020-03-18 13:07:26 +0100

    Fjernet test change

--- Scraper/scraper.js
+++ Scraper/scraper.js
@@ -21,7 +21,7 @@ async function scraperProduct(url){
 
     browser.close();
 }
-/*Test*/
+
 scraperProduct('https://www.ryanair.com/dk/da/trip/flights/select?adults=1&teens=0&children=0&infants=0&dateOut=2020-04-16&dateIn=2020-04-23&originIata=CPH&destinationIata=STN&isConnectedFlight=false&isReturn=true&discount=0&tpAdults=1&tpTeens=0&tpChildren=0&tpInfants=0&tpStartDate=2020-04-16&tpEndDate=2020-04-23&tpOriginIata=CPH&tpDestinationIata=STN&tpIsConnectedFlight=false&tpIsReturn=true&tpDiscount=0');
 
 
