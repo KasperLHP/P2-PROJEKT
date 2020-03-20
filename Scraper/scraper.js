250b21dcd774d5576e69b52e4e04f7975adbc7d8
Kasper Leander Helmer Pedersen <kasper0207@gmail.com>
2020-03-20 11:42:15 +0100

    Updated times

--- Scraper/scraper.js
+++ Scraper/scraper.js
@@ -18,22 +18,42 @@ async function scraperProduct(url){
     const [el3] = await page.$x('/html/body/flights-root/div/div/flights-trip-details-container/flights-trip-details/div/div[2]/text()[2]');
     const txt3 = await el3.getProperty('textContent');
     const DepartureDate = await txt3.jsonValue();
-
+    //Departure time
+    const [el4] = await page.$x('/html/body/flights-root/div/div/div/div/flights-summary-container/flights-summary/div/div[1]/journey-container/journey/flight-list/div/flight-card[3]/div/div/div[1]/div/flight-info/div[1]/span[1]');
+    const txt4 = await el4.getProperty('textContent');
+    const DepartureTime = await txt4.jsonValue();
+    //Arrival time
+    const [el5] = await page.$x('/html/body/flights-root/div/div/div/div/flights-summary-container/flights-summary/div/div[1]/journey-container/journey/flight-list/div/flight-card[3]/div/div/div[1]/div/flight-info/div[3]/span[1]');
+    const txt5 = await el5.getProperty('textContent');
+    const ArrivalTime = await txt5.jsonValue();
     //Return - gives price and from city to city
     //Price
-    const [el4] = await page.$x('/html/body/flights-root/div/div/div/div/flights-summary-container/flights-summary/div/div[2]/journey-container/journey/div/div[2]/carousel-container/carousel/div/ul/li[3]/carousel-item/button/div[2]/ry-price/span[2]');
-    const txt4 = await el4.getProperty('textContent');
-    const price2 = await txt4.jsonValue();
-    //From city, to city
-    const [el5] = await page.$x('/html/body/flights-root/div/div/div/div/flights-summary-container/flights-summary/div/div[2]/journey-container/journey/div/div[1]/h3/text()');
-    const txt5 = await el5.getProperty('textContent');
-    const FromTo2 = await txt5.jsonValue();
-    //Return date
-    const [el6] = await page.$x('/html/body/flights-root/div/div/flights-trip-details-container/flights-trip-details/div/div[2]/span[2]');
+    const [el6] = await page.$x('/html/body/flights-root/div/div/div/div/flights-summary-container/flights-summary/div/div[2]/journey-container/journey/div/div[2]/carousel-container/carousel/div/ul/li[3]/carousel-item/button/div[2]/ry-price/span[2]');
     const txt6 = await el6.getProperty('textContent');
-    const ReturnDate = await txt6.jsonValue();
+    const price2 = await txt6.jsonValue();
+    //From city, to city
+    const [el7] = await page.$x('/html/body/flights-root/div/div/div/div/flights-summary-container/flights-summary/div/div[2]/journey-container/journey/div/div[1]/h3/text()');
+    const txt7 = await el7.getProperty('textContent');
+    const FromTo2 = await txt7.jsonValue();
+    //Return date
+    const [el8] = await page.$x('/html/body/flights-root/div/div/flights-trip-details-container/flights-trip-details/div/div[2]/span[2]');
+    const txt8 = await el8.getProperty('textContent');
+    const ReturnDate = await txt8.jsonValue();
+    //Departure time
+    const [el9] = await page.$x('/html/body/flights-root/div/div/div/div/flights-summary-container/flights-summary/div/div[2]/journey-container/journey/flight-list/div/flight-card[1]/div/div/div[1]/div/flight-info/div[1]/span[1]');
+    const txt9 = await el9.getProperty('textContent');
+    const DepartureTime2 = await txt9.jsonValue();
+    //Arrival time
+    const [el10] = await page.$x('/html/body/flights-root/div/div/div/div/flights-summary-container/flights-summary/div/div[2]/journey-container/journey/flight-list/div/flight-card[1]/div/div/div[1]/div/flight-info/div[3]/span[1]');
+    const txt10 = await el10.getProperty('textContent');
+    const ArrivalTime2 = await txt10.jsonValue();
 
-    console.log({Departure: FromTo, DepartureDate: DepartureDate + " 2020", Price: price}, {Return: FromTo2, ReturnDate: ReturnDate.slice(3, 10) + " 2020", Price: price2}, {ScrapeDate: Date().toLocaleString()});
+
+
+
+    console.log({Departure: FromTo, DepartureDate: DepartureDate + " 2020", Price: price, DepartureTime: DepartureTime, ArrivalTime: ArrivalTime}, 
+                {Return: FromTo2, ReturnDate: ReturnDate.slice(3, 10) + " 2020", Price: price2, DepartureTime: DepartureTime2, ArrivalTime: ArrivalTime2}, 
+                {ScrapeDate: Date().toLocaleString()});
     browser.close();
 }
 
