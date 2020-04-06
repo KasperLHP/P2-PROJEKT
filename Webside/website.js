3ef4e95aedc866fa59e8d70194e57aca921eadbf
J-Sondergaard <57714653+J-Sondergaard@users.noreply.github.com>
2020-04-06 14:03:30 +0200

    update

--- Webside/website.js
+++ Webside/website.js
@@ -1,7 +1,7 @@
 //Destination
 function autocomplete(inp, arr) {
     var currentFocus;
-    inp.addEventListener("input", function(e) {
+    inp.addEventListener("myInput", function(e) {
         var a, b, i, val = this.value;
         closeAllLists();
         if (!val) { return false;}
@@ -19,7 +19,7 @@ function autocomplete(inp, arr) {
             b.innerHTML += arr[i].substr(val.length);
             b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
             b.addEventListener("click", function(e) {
-                inp.value = this.getElementsByTagName("input")[0].value;
+                inp.value = this.getElementsByTagName("myInput")[0].value;
                 closeAllLists();
             });
             a.appendChild(b);
@@ -89,184 +89,13 @@ $('#form-autocomplete-4').mdbAutocomplete({
     inputFocusShadow: '0 1px 0 0 #4285f4',
     inputBlurShadow: ''
     });
-
+/*
 function rejseDestination() {
     let inputFra = document.getElementById("inputFra").value;
     let inputTil = document.getElementById("inputTil").value;
 }
-
-//Date Picker code
-const date_picker_element = document.querySelector('.date-picker');
-const selected_date_element = document.querySelector('.date-picker .selected-date');
-const dates_element = document.querySelector('.date-picker .dates');
-
-const mth_element = document.querySelector('.date-picker .dates .month .mth');
-const next_mth_element = document.querySelector('.date-picker .dates .month .next-mth');
-const prev_mth_element = document.querySelector('.date-picker .dates .month .prev-mth');
-const days_element = document.querySelector('.date-picker .dates .days');
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-let date = new Date();
-let day = date.getDate();
-let month = date.getMonth();
-let year = date.getFullYear();
-
-let selectedDate = date;
-let selectedDay = day;
-let selectedMonth = month;
-let selectedYear = year;
-
-mth_element.textContent = months[month] + ' ' + year;
-
-selected_date_element.textContent = formatDate(date);
-
-populateDates();
-
-// EVENT LISTENERS
-date_picker_element.addEventListener('click', toggleDatePicker);
-next_mth_element.addEventListener('click', goToNextMonth);
-prev_mth_element.addEventListener('click', goToPrevMonth);
-
-//FUNCTIONS
-function toggleDatePicker (e) {
-    if (!checkEventPathForClass(e.path, 'dates')){
-        dates_element.classList.toggle('active');
-    }
-}
-
-function goToNextMonth (e) {
-    month++;
-    if (month > 11) {
-        month = 0;
-        year++;
-    }
-    mth_element.textContent = months[month] + ' ' + year;
-}
-
-function goToPrevMonth (e) {
-    month--;
-    if (month < 0) {
-        month = 11;
-        year--;
-    }
-    mth_element.textContent = months[month] + ' ' + year;
-}
-
-function populateDates (e) {
-    days_element.innerHTML = '';
-    let amount_days = 31;
-
-    if (month ==1) {
-        amount_days = 28;
-    }
-
-    for (let i = 0; i < amount_days; i++){
-        const day_element = document.createElement('div');
-        day_element.classList.add('day');
-        day_element.textContent = i + 1;
-
-        days_element.appendChild(day_element);
-    }
-}
-
-// HELPER FUNCTIONS
-function checkEventPathForClass (path, selector) {
-    for (let i = 0; i < path.length; i++) {
-        if (path[i].classList && path[i].classList.contains(selector)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function formatDate (d) {
-    let day = d.getDate();
-    if (day < 10) {
-        day = '0' + day;
-    }
-
-    let month = d.getMonth() + 1;
-    if (month < 10) {
-        month = '0' + month;
-    }
-
-    let year = d.getFullYear();
-    if (year < 10) {
-        year = '0' + year;
-    }
-
-    return year + ' / ' + month + ' / ' + day;
-}
-
-function aldersgruppe(){
-    var x,i,j,selElmnt,a,b,c;
-
-    x = document.getElementsByClassName("custom-select");
-    for (i = 0; i < x.length; i++){
-        selElmnt = x[i].getElementsByTagName("select")[0];
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-items select-hide");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-
-    /*Opretter en DIV der indeholder valgmulighederne*/
-
-    b = document.createElement("DIV");
-    b.setAttribute("class,select-items select-hide");
-    for (j = 1; j < selElmnt.length; j++){
-        c = document.createElement("DIV");
-        c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function(e) {
-     var y, i, k, s, h;
-    s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-    h = this.parentNode.previousSibling;
-    for (i = 0; i < s.length; i++) {
-        if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            for (k = 0; k < y.length; k++) {
-               y[k].removeAttribute("class");
-            }
-               this.setAttribute("class", "same-as-selected");
-                break;
-              }
-            }
-            h.click();
-        });
-        b.appendChild(c);
-      }
-      x[i].appendChild(b);
-      a.addEventListener("click", function(e) {
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-      });
-    }
+*/
 
 
-    function closeAllSelect(elmnt) {
-        /* Funktion der lukker alle åbnet boxe */
-        var x, y, i, arrNo = [];
-        x = document.getElementsByClassName("select-items");
-        y = document.getElementsByClassName("select-selected");
-        for (i = 0; i < y.length; i++) {
-          if (elmnt == y[i]) {
-            arrNo.push(i)
-          } else {
-            y[i].classList.remove("select-arrow-active");
-          }
-        }
-        for (i = 0; i < x.length; i++) {
-          if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-          }
-        }
-      }
-      
-      /* Hvis man klikker andre steder end boxen så lukker den ned*/
-      document.addEventListener("click", closeAllSelect);
-}
 
 
